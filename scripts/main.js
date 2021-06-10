22496f5ed6b0f3c10cf12edb9c197ad8a6783030
Natalie <nataliech0409@gmail.com>
2021-06-10 13:41:03 +0300

    Refactoring

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+/* Toggle text or figure */
+
+// const textFigureTrigger = document.querySelector('#text-trigger');
+// const figureTextTrigger = document.querySelector('#figure-trigger');
+// const figureTools = document.querySelector('.figure-tools');
+// const textTools = document.querySelector('.text-tools');
+
+
+// function toggleCanvas () {
+//     if(figureTextTrigger.value == 'text') {
+//         figureTools.classList.remove('active');
+//         figureTools.classList.add('disabled');
+//         textTools.classList.remove('disabled');
+//         textTools.classList.add('active')   
+//     } else {
+
+//     }
+// }
+
+// figureTextTrigger.addEventListener('change', toggleCanvas )
