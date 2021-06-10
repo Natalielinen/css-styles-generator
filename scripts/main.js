9dd2980e19955b33753412e3a8195d6dab3c3bb7
Natalie <nataliech0409@gmail.com>
2021-06-10 15:07:34 +0300

    Day 3: added some functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,46 @@
-/* Toggle text or figure */
+/* Choose canvas */
+const canvasTrigger = document.querySelector('#canvas-trigger');
+const figureCanvas = document.querySelector('.figure-canvas');
+const textCanvas = document.querySelector('.text-canvas');
 
-// const textFigureTrigger = document.querySelector('#text-trigger');
-// const figureTextTrigger = document.querySelector('#figure-trigger');
-// const figureTools = document.querySelector('.figure-tools');
-// const textTools = document.querySelector('.text-tools');
+function toggleCanvas () {
+    figureCanvas.classList.toggle('disabled');
+    textCanvas.classList.toggle('disabled');
+};
 
+canvasTrigger.addEventListener('change', toggleCanvas);
 
-// function toggleCanvas () {
-//     if(figureTextTrigger.value == 'text') {
-//         figureTools.classList.remove('active');
-//         figureTools.classList.add('disabled');
-//         textTools.classList.remove('disabled');
-//         textTools.classList.add('active')   
-//     } else {
+/* Accordeon */
+const accordeonItemsTitles = document.querySelectorAll('.accordeon__item__title');
+const accordeonItemsContent = document.querySelectorAll('.accordeon__item__content');
 
-//     }
-// }
+for (let item of accordeonItemsTitles) {
+    item.addEventListener('click', function () {
+        this.nextElementSibling.classList.toggle('disabled');       
+    });
+};
 
-// figureTextTrigger.addEventListener('change', toggleCanvas )
+/* Square customization */
+
+/* Square color */
+
+const squareColor = document.getElementById('square-color');
+const square = document.querySelector('.main-content__canvas__square');
+
+function changeSquareColor () {
+    square.style.backgroundColor = squareColor.value;
+}
+
+squareColor.addEventListener('change', changeSquareColor);
+
+/* Square opacity */
+
+const opacityRange = document.querySelector('#square-opacity');
+const opacityRangeSpan = document.querySelector('#square-opacity+span');
+
+function changeOpacity () {
+    square.style.opacity = opacityRange.value;
+    opacityRangeSpan.innerHTML = opacityRange.value;
+};
+
+opacityRange.addEventListener('input', changeOpacity);
