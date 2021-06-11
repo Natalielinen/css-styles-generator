5b0543686fd8cff98ff5b9c290cff26bfc4019c9
Natalie <nataliech0409@gmail.com>
2021-06-11 16:41:30 +0300

    Day 4: borders and shadows change functions

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const canvasTrigger = document.querySelector('#canvas-trigger');
 const figureCanvas = document.querySelector('.figure-canvas');
 const textCanvas = document.querySelector('.text-canvas');
 
-function toggleCanvas () {
+function toggleCanvas() {
     figureCanvas.classList.toggle('disabled');
     textCanvas.classList.toggle('disabled');
 };
@@ -16,7 +16,7 @@ const accordeonItemsContent = document.querySelectorAll('.accordeon__item__conte
 
 for (let item of accordeonItemsTitles) {
     item.addEventListener('click', function () {
-        this.nextElementSibling.classList.toggle('disabled');       
+        this.nextElementSibling.classList.toggle('disabled');
     });
 };
 
@@ -28,12 +28,15 @@ for (let item of accordeonItemsTitles) {
 
 const squareColor = document.getElementById('square-color');
 const square = document.querySelector('.main-content__canvas__square');
+const answerColor = document.getElementById('background-color');
 
-function changeSquareColor () {
+
+function changeSquareColor() {
     square.style.backgroundColor = squareColor.value;
+    answerColor.innerHTML = squareColor.value;
 }
 
-squareColor.addEventListener('change', changeSquareColor);
+squareColor.addEventListener('input', changeSquareColor);
 
 /* Square opacity */
 
@@ -41,7 +44,7 @@ const opacityRange = document.querySelector('#square-opacity');
 const opacityRangeSpan = document.querySelector('#square-opacity+span');
 const squareAnswerOpacitySpan = document.getElementById('answer-square-opasity');
 
-function changeOpacity () {
+function changeOpacity() {
     square.style.opacity = opacityRange.value;
     opacityRangeSpan.innerHTML = opacityRange.value;
     squareAnswerOpacitySpan.innerHTML = opacityRange.value;
@@ -56,15 +59,15 @@ const squareHeightInput = document.getElementById('square-height');
 const squareAnswerWidthSpan = document.getElementById('answer-square-width');
 const squareAnswerHeightSpan = document.getElementById('answer-square-height');
 
-function changeWidth () {
+function changeWidth() {
     square.style.width = squareWidthInput.value + 'px';
     squareAnswerWidthSpan.innerHTML = squareWidthInput.value;
-}
+};
 
-function changeHeight () {
-    square.style.height = squareHeightInput.value +'px';
+function changeHeight() {
+    square.style.height = squareHeightInput.value + 'px';
     squareAnswerHeightSpan.innerHTML = squareHeightInput.value;
-}
+};
 
 squareWidthInput.addEventListener('input', changeWidth);
 squareHeightInput.addEventListener('input', changeHeight);
@@ -87,35 +90,129 @@ const bottomLeftResult = document.getElementById('bl-result');
 //span-answer
 const borderRadiusAnswer = document.getElementById('square-border-radius');
 
-
-let borderRadius = topLeftRange.value + 'px ' + 
-                   topRightRange.value + 'px ' +
-                   bottomRightRange.value + 'px ' +
-                   bottomLeftRange.value + 'px';
-
-function getRadius () {
+function changeRadius() {
     topLeftResult.innerHTML = topLeftRange.value;
     topRightResult.innerHTML = topRightRange.value;
     bottomRightResult.innerHTML = bottomRightRange.value;
     bottomLeftResult.innerHTML = bottomLeftRange.value;
 
-    square.style.borderRadius = 
-    topLeftRange.value + 'px ' + 
-    topRightRange.value + 'px ' +
-    bottomRightRange.value + 'px ' +
-    bottomLeftRange.value + 'px';
+    let borderRadiusParams = topLeftRange.value + 'px ' +
+        topRightRange.value + 'px ' +
+        bottomRightRange.value + 'px ' +
+        bottomLeftRange.value + 'px';
 
-    borderRadiusAnswer.innerHTML = topLeftRange.value + 'px ' + 
-    topRightRange.value + 'px ' +
-    bottomRightRange.value + 'px ' +
-    bottomLeftRange.value + 'px';
+    square.style.borderRadius = borderRadiusParams;
 
-
-}  
+    borderRadiusAnswer.innerHTML = borderRadiusParams;
+};
 
 for (let elem of allRadiusInputs) {
-    elem.addEventListener('input', getRadius)
+    elem.addEventListener('input', changeRadius);
+};
+
+/* Box shadow */
+
+const insetShadowCheckbox = document.getElementById('inset-box-shadow');
+
+const boxShadowOffsetX = document.getElementById('box-shadow-offset-x');
+const boxShadowOffsetY = document.getElementById('box-shadow-offset-y');
+const boxShadowBlur = document.getElementById('box-shadow-blur');
+const boxShadowSpread = document.getElementById('box-shadow-spread');
+const boxShadowColor = document.getElementById('box-shadow-color');
+const boxShadowInputs = document.querySelectorAll('.box-shadow-input');
+
+const boxShadowAnswer = document.getElementById('box-shadow-answer');
+
+function changeBoxShadow() {
+    
+    boxShadowOffsetX.nextElementSibling.innerHTML = boxShadowOffsetX.value;
+    boxShadowOffsetY.nextElementSibling.innerHTML = boxShadowOffsetY.value;
+    boxShadowBlur.nextElementSibling.innerHTML = boxShadowBlur.value;
+    boxShadowSpread.nextElementSibling.innerHTML = boxShadowSpread.value;
+
+    let shadowParams = `${insetShadowCheckbox.checked  ? 'inset' : ''} 
+    ${boxShadowOffsetX.value}px 
+    ${boxShadowOffsetY.value}px 
+    ${boxShadowBlur.value}px 
+    ${boxShadowSpread.value}px 
+    ${boxShadowColor.value}`;
+
+    boxShadowAnswer.innerHTML = shadowParams;
+
+    square.style.boxShadow = shadowParams;
+};
+
+for (let input of boxShadowInputs) {
+    input.addEventListener('input', changeBoxShadow);
+};
+
+insetShadowCheckbox.addEventListener('change', changeBoxShadow);
+
+/* Borders (square) */
+
+const topBlockBorder = document.getElementById('top-block-border');
+const rightBlockBorder = document.getElementById('right-block-border');
+const bottomBlockBorder = document.getElementById('bottom-block-border');
+const leftBlockBorder = document.getElementById('left-block-border');
+const blockBorderRanges = document.querySelectorAll('.block-border-range');
+const allBorderCheckbox = document.getElementById('all-block-borders');
+
+const blockBorderWidth = document.getElementById('block-border-width');
+const blockBorderStyle = document.getElementById('block-border-style');
+const blockBorderColor = document.getElementById('block-border-color');
+
+const blockBorderPostfix = document.getElementById('block-border-postfix');
+const blockBorderAnswer = document.getElementById('block-border-answer');
+
+
+function changeBlockBorder() {
+    for (let range of blockBorderRanges) {
+        range.nextElementSibling.innerHTML = range.value;
+    };
+};
+
+function disableRanges () {
+    if(allBorderCheckbox.checked) {
+        //сделать через цикл и переключение класса
+        topBlockBorder.disabled = true;
+        topBlockBorder.style.opacity = '0.5';
+        rightBlockBorder.disabled = true;
+        rightBlockBorder.style.opacity = '0.5';
+        bottomBlockBorder.disabled = true;
+        bottomBlockBorder.style.opacity = '0.5';
+        leftBlockBorder.disabled = true;
+        leftBlockBorder.style.opacity = '0.5';
+        blockBorderWidth.disabled = false;
+        blockBorderWidth.style.opacity = '1';
+    } else {
+        topBlockBorder.disabled = false;
+        topBlockBorder.style.opacity = '1';
+        rightBlockBorder.disabled = false;
+        rightBlockBorder.style.opacity = '1';
+        bottomBlockBorder.disabled = false;
+        bottomBlockBorder.style.opacity = '1';
+        leftBlockBorder.disabled = false;
+        leftBlockBorder.style.opacity = '1';
+        blockBorderWidth.disabled = true;
+        blockBorderWidth.style.opacity = '0.5';
+    }
 }
 
+function changeAllBorders () {
+    square.style.border = 
+    blockBorderWidth.value + 'px ' + 
+    blockBorderStyle.value + ' ' + 
+    blockBorderColor.value;
+
+}
+
+for (let range of blockBorderRanges) {
+    range.addEventListener('input', changeBlockBorder);
+};
+
+allBorderCheckbox.addEventListener('change', disableRanges);
+blockBorderWidth.addEventListener('input', changeAllBorders);
+blockBorderStyle.addEventListener('change', changeAllBorders);
+blockBorderColor.addEventListener('input', changeAllBorders);
 
 
